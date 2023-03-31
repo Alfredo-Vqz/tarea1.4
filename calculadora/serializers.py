090315e08a6cec841ad5ce3608b59b0e169710f8
Alfredo-Vqz <a01656384@tec.mx>
2023-03-31 14:28:37 -0600

    tarea

---
+++ calculadora/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from . models import Reto, Jugadores, Usuario, partidas
+
+
+###############################################
+############  TAREA 1.4 REST ##################
+###############################################
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('id', 'password')
+
+class PartidasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = partidas
+        fields = ('id', 'fecha', 'usuario', 'minutos_jugados', 'puntaje')
+
+###############################################
+
+
+class RetoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Reto
+        fields = ('id','nombre','minutos_jugados')
+
+class JugadorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Jugadores
+        fields = ('id','grupo','num_lista')
